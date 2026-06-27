d38a2836ba5b88aff8fd0be8449bc8aea055603d
Alexofthefoot <alex23thefoot@gmail.com>
2026-06-26 20:10:54 -0700

    Implemented position names to reading creation page

--- frontend/js/newReading.js
+++ frontend/js/newReading.js
@@ -1,38 +1,65 @@
-let NUMBER_OF_CARDS = 5;
+const DEFAULT_POSITIONS = ['Past', 'Present', 'Future'];
+const DEFAULT_NUMBER_OF_CARDS = 3;
+let CURRENT_NUMBER_OF_CARDS = 3;
 
 function setButtons(num) {
-    if (num == NUMBER_OF_CARDS) {
+    if (num == CURRENT_NUMBER_OF_CARDS) {
         return;
     }
-    else if (num > NUMBER_OF_CARDS){
-        addbuttons(num - NUMBER_OF_CARDS);
+    else if (num > CURRENT_NUMBER_OF_CARDS) {
+        addbuttons(num - CURRENT_NUMBER_OF_CARDS);
     }
     else {
-        removebuttons(NUMBER_OF_CARDS - num);
+        removebuttons(CURRENT_NUMBER_OF_CARDS - num);
     }
-    NUMBER_OF_CARDS = num;
+    CURRENT_NUMBER_OF_CARDS = num;
 }
 
 //adds the desired number of buttons to HTML structure
 function addbuttons(num) {
-    const parentDiv = document.getElementsByClassName("card-button-row")[0];
-    for (let i = 0; i < num; i++){
+    const parentDiv = document.getElementById("spreadBoard");
+    for (let i = 0; i < num; i++) {
+        const newDiv = document.createElement("div");
         const newButton = document.createElement("button");
+        const newImg = document.createElement("img");
+        const newSubDiv = document.createElement("div");
+
+        newDiv.className = "spread-slot";
         newButton.type = "button";
         newButton.className = "card-selection-btn";
-        const newImg = document.createElement("img");
         newImg.src = "../frontend/assets/Cards-png/CardBacks.png";
-        
-        // newButton.onclick = 
+        newSubDiv.className = "card-position-label";
+        newSubDiv.contentEditable = "true";
+
+        newDiv.append(newButton);
+        newDiv.append(newSubDiv);
         newButton.append(newImg);
-        parentDiv.append(newButton);
+        parentDiv.append(newDiv);
     }
 }
 
 //removes the desired number of buttons from HTML structure
 function removebuttons(num) {
-    const parentDiv = document.getElementsByClassName('card-button-row')[0];
-    for (let i = 0; i < num; i++){
+    const parentDiv = document.getElementById("spreadBoard");
+    for (let i = 0; i < num; i++) {
         parentDiv.children[parentDiv.children.length - 1].remove();
     }
 }
+
+function clearPositions() {
+    const labels = document.getElementsByClassName("card-position-label");
+    for (let i = 0; i < CURRENT_NUMBER_OF_CARDS && i < DEFAULT_POSITIONS.length; i++) {
+        labels[i].innerHTML = "";
+    }
+}
+
+//should this reset card number too?
+function resetPositions() {
+    const labels = document.getElementsByClassName("card-position-label");
+    for (let i = 0; i < CURRENT_NUMBER_OF_CARDS && i < DEFAULT_POSITIONS.length; i++) {
+        labels[i].innerHTML = DEFAULT_POSITIONS[i];
+    }
+}
+
+
+
