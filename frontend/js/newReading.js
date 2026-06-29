81bbffbc06c83df0f206e994b3965a8c8144b9ef
Alexofthefoot <alex23thefoot@gmail.com>
2026-06-28 21:37:16 -0700

    Implement modal window in reading creation page

--- frontend/js/newReading.js
+++ frontend/js/newReading.js
@@ -2,21 +2,68 @@ const DEFAULT_POSITIONS = ['Past', 'Present', 'Future'];
 const DEFAULT_NUMBER_OF_CARDS = 3;
 let CURRENT_NUMBER_OF_CARDS = 3;
 
-function setButtons(num) {
+function setupEventHandlers() {
+    //The button-row buttons
+    const parentDiv = document.getElementById("number-button-row");
+    for (let i = 0; i < parentDiv.children.length; i++) {
+        const button = parentDiv.children[i];
+        button.addEventListener("click", () => {
+            changeNumberofCards(i + 1);
+            console.log('firing the set buttons event for ', i + 1);
+        });
+    }
+    // The spread board card/buttons
+    const spreadBoard = document.getElementById('spreadBoard');
+    for (let i = 0; i < spreadBoard.children.length; i++) {
+        const idVal = 'spread-' + (i + 1);
+        const btn = document.getElementById(idVal);
+        btn.addEventListener("click", () => {
+            openModal(i + 1);
+        });
+    }
+    // Closing the modal window
+    const modal = document.getElementById("modal-window");
+    const span = document.getElementById('close-modal');
+    span.addEventListener("click", () => {
+        modal.style.display = "none";
+        console.log('closing modal window via span event listener');
+    });
+    window.addEventListener("click", (event) => {
+        if (event.target == modal) {
+            modal.style.display = "none";
+            console.log('closing modal window via window click event listener');
+        }
+    });
+    // Card position buttons
+     const resetBtn = document.getElementById('reset-position');
+     resetBtn.addEventListener("click", resetPositions);
+     const clearBtn = document.getElementById('clear-position');
+     clearBtn.addEventListener("click", clearPositionNames);
+    // Submitting the form
+    const form = document.getElementById('reading-form');
+    form.addEventListener("submit", async function (event) {
+        event.preventDefault(); // Stop reload
+        console.log('submit button');
+
+    });
+}
+
+// Adjusts number of card buttons up or down
+function changeNumberofCards(num) {
     if (num == CURRENT_NUMBER_OF_CARDS) {
         return;
     }
     else if (num > CURRENT_NUMBER_OF_CARDS) {
-        addbuttons(num - CURRENT_NUMBER_OF_CARDS);
+        addCards(num - CURRENT_NUMBER_OF_CARDS);
     }
     else {
-        removebuttons(CURRENT_NUMBER_OF_CARDS - num);
+        removeCards(CURRENT_NUMBER_OF_CARDS - num);
     }
     CURRENT_NUMBER_OF_CARDS = num;
 }
 
-//adds the desired number of buttons to HTML structure
-function addbuttons(num) {
+// Adds the desired number of buttons to HTML structure
+function addCards(num) {
     const parentDiv = document.getElementById("spreadBoard");
     for (let i = 0; i < num; i++) {
         const newDiv = document.createElement("div");
@@ -24,9 +71,15 @@ function addbuttons(num) {
         const newImg = document.createElement("img");
         const newSubDiv = document.createElement("div");
 
+        const idVal = CURRENT_NUMBER_OF_CARDS + 1 + i;
+
         newDiv.className = "spread-slot";
         newButton.type = "button";
         newButton.className = "card-selection-btn";
+        newButton.id = "spread-" + idVal;
+        newButton.addEventListener("click", () => {
+            openModal(idVal);
+        });
         newImg.src = "../frontend/assets/Cards-png/CardBacks.png";
         newSubDiv.className = "card-position-label";
         newSubDiv.contentEditable = "true";
@@ -38,22 +91,23 @@ function addbuttons(num) {
     }
 }
 
-//removes the desired number of buttons from HTML structure
-function removebuttons(num) {
+// Removes the desired number of buttons from HTML structure
+function removeCards(num) {
     const parentDiv = document.getElementById("spreadBoard");
     for (let i = 0; i < num; i++) {
         parentDiv.children[parentDiv.children.length - 1].remove();
     }
 }
 
-function clearPositions() {
+function clearPositionNames() {
     const labels = document.getElementsByClassName("card-position-label");
     for (let i = 0; i < CURRENT_NUMBER_OF_CARDS && i < DEFAULT_POSITIONS.length; i++) {
         labels[i].innerHTML = "";
     }
 }
 
-//should this reset card number too?
+// TODO:
+// Should this reset card number too?
 function resetPositions() {
     const labels = document.getElementsByClassName("card-position-label");
     for (let i = 0; i < CURRENT_NUMBER_OF_CARDS && i < DEFAULT_POSITIONS.length; i++) {
@@ -61,5 +115,18 @@ function resetPositions() {
     }
 }
 
+// Open the card-picker modal window
+function openModal(cardID) {
+    const modal = document.getElementById('modal-window');
+    console.log("opening modal for card " + cardID);
+    modal.style.display = "block";
+}
 
 
+
+
+
+
+window.onload = function () {
+    setupEventHandlers();
+};
